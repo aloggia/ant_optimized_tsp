6f1fe680ba0093b960a4c1db53e333aef16a48ef
aloggia <aloggia22@gmail.com>
2022-05-16 20:25:33 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+/*
+TODO: either create or import a graph library
+ */
+
+/*
+TODO: Code algorithm
+ Algorithm idea: Pick starting node
+ Create array of all possible destinations from that node
+ Weight each edge for picking -> all edges have a distance/cost, but we need to weight them so the
+ ant randomly picks an edge to take: Higher the distance, higher the weight
+ When a node is visited, mark as visited so the ant isn't doubling back on itself
+ Save the path the ant took as an array listing all visited nodes in order of visit
+    - Because the ant can only travel on edges, the path will always be valid
+    - The total cost of the path is 
+ */
+
+
+fn main() {
+    println!("Hello, world!");
+}
