1c1584c0d0aca6c8a39d78a5f80421fd1443c20c
aloggia <aloggia22@gmail.com>
2022-05-16 20:41:57 -0400

    Project outlining

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,22 @@ TODO: Code algorithm
  When a node is visited, mark as visited so the ant isn't doubling back on itself
  Save the path the ant took as an array listing all visited nodes in order of visit
     - Because the ant can only travel on edges, the path will always be valid
-    - The total cost of the path is 
+    - The total cost of the path is the sum of the weights of all traveled edges
  */
 
-
+// TODO: destination = distances[curr_idx, pot_nxt_idx]
+// TODO: desirability = pow(1/destination, dst_power) * pow(pheromone_str, pheremone_pow)
+// TODO: Multi-thread the ants -> run multiple ants at once
+// TODO: pheremone_str = pheremoneTrails[curr_idx, pot_nxt_idx]
+/*
+TODO: parameters:
+    - dst_power
+    - pheremone_str
+    - pheremone_intensity
+    - initial_pheremone_intensity
+    - evaporation_rate
+    - num_ants
+ */
 fn main() {
     println!("Hello, world!");
 }
